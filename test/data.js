64e95f334c059ce5520d607b66b2049ceda02243
dolymood <dolymood@gmail.com>
2012-11-24 11:10:54 +0800

    modify test data.js

--- test/data.js
+++ test/data.js
@@ -8,7 +8,7 @@ $(function() {
 			var body = _$(document.body);
 			body.data('test_data', '2012,11,24,0:54');
 			equal(body.data('test_data'), '2012,11,24,0:54');
-			body.removeData('test_data');
+			equal(body.removeData('test_data'), '2012,11,24,0:54');
 			equal(body.data('test_data'));
 			start();
 		});
