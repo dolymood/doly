c9d0a6d17fb5c8931c1a4485a9fea69737df3f6f
dolymood <dolymood@gmail.com>
2012-11-24 18:36:54 +0800

    add query.js

---
+++ query.js
@@ -0,0 +1,24 @@
+/**
+ * module query
+ */
+
+define('query', ['$sizzle'], function(Sizzle) {
+    'use strict';
+	
+	var query = {
+	    
+		find: Sizzle,
+		text: Sizzle.getText,
+		expr: Sizzle.selectors,
+		unique: Sizzle.uniqueSort,
+		matches: Sizzle.matches,
+		isXMLDoc: Sizzle.isXML,
+		eleContains: Sizzle.contains
+		matchesSelector: Sizzle.matchesSelector
+		
+	};
+	
+	query.expr[':'] = query.expr.pseudos;
+	doly.mixin(doly, query);
+	return query;
+});
