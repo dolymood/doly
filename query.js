09e7697da5eda6be3bd14f64ba73cda74e73769c
dolymood <dolymood@gmail.com>
2012-11-24 18:37:16 +0800

    modify query.js

--- query.js
+++ query.js
@@ -4,21 +4,21 @@
 
 define('query', ['$sizzle'], function(Sizzle) {
     'use strict';
-	
-	var query = {
-	    
-		find: Sizzle,
-		text: Sizzle.getText,
-		expr: Sizzle.selectors,
-		unique: Sizzle.uniqueSort,
-		matches: Sizzle.matches,
-		isXMLDoc: Sizzle.isXML,
-		eleContains: Sizzle.contains
-		matchesSelector: Sizzle.matchesSelector
-		
-	};
-	
-	query.expr[':'] = query.expr.pseudos;
-	doly.mixin(doly, query);
-	return query;
+    
+    var query = {
+        
+        find: Sizzle,
+        text: Sizzle.getText,
+        expr: Sizzle.selectors,
+        unique: Sizzle.uniqueSort,
+        matches: Sizzle.matches,
+        isXMLDoc: Sizzle.isXML,
+        eleContains: Sizzle.contains
+        matchesSelector: Sizzle.matchesSelector
+        
+    };
+    
+    query.expr[':'] = query.expr.pseudos;
+    doly.mixin(doly, query);
+    return query;
 });
