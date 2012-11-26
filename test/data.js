38393940a324ba5817e8065da1c50e6611a68758
dianmiao <dianmiao@sohu-inc.com>
2012-11-26 15:26:51 +0800

    modify test data.js

--- test/data.js
+++ test/data.js
@@ -5,11 +5,11 @@ $(function() {
 		
 		_$.require(['$data'], function() {
 		    
-			var body = _$(document.body);
-			body.data('test_data', '2012,11,24,0:54');
-			equal(body.data('test_data'), '2012,11,24,0:54');
-			equal(body.removeData('test_data'), '2012,11,24,0:54');
-			equal(body.data('test_data'));
+			var body = document.body;
+			_$.data(body, 'test_data', '2012,11,24,0:54');
+			equal(_$.data(body, 'test_data'), '2012,11,24,0:54');
+			equal(_$.removeData(body, 'test_data'), '2012,11,24,0:54');
+			equal(_$.data(body, 'test_data'));
 			start();
 		});
 	});
