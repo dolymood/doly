0ea008a4ada0b67e79882ca6e753f31ce228a2af
dianmiao <dianmiao@sohu-inc.com>
2012-12-06 16:22:11 +0800

    modify promise

--- promise.js
+++ promise.js
@@ -4,34 +4,35 @@
 
 define('promise', ['$class'], function() {
     'use strict';
-	
-	var promiseCache = {},
-	    PENDING = 1,
-		RESOLVED = 2,
-		REJECTED = 3,
-		_rnds = new Array(16),
-		_rng = function() {
-		    for (var i = 0, r; i < 16; i++) {
-			    if ((i & 0x03) === 0) r = Math.random() * 0x100000000;
-			    _rnds[i] = r >>> ((i & 0x03) << 3) & 0xff;
-		    }
-		    return _rnds;
-		},
-		_byteToHex = (function() {
-		    var ret = [];
-			for (var i = 0; i < 256; i++) {
-				ret[i] = (i + 0x100).toString(16).substr(1);
-			}
-			return ret;
-		})(),
-		// https://github.com/broofa/node-uuid
-		uuid = function() {
-		    var i = 0,
-			    rnds = _rng(),
-			    bth = _byteToHex;
-			rnds[6] = (rnds[6] & 0x0f) | 0x40;
+    
+    var promiseCache = {},
+        PENDING = 1,
+        RESOLVED = 2,
+        REJECTED = 3,
+        each = doly.each,
+        _rnds = new Array(16),
+        _rng = function() {
+            for (var i = 0, r; i < 16; i++) {
+                if ((i & 0x03) === 0) r = Math.random() * 0x100000000;
+                _rnds[i] = r >>> ((i & 0x03) << 3) & 0xff;
+            }
+            return _rnds;
+        },
+        _byteToHex = (function() {
+            var ret = [];
+            for (var i = 0; i < 256; i++) {
+                ret[i] = (i + 0x100).toString(16).substr(1);
+            }
+            return ret;
+        })(),
+        // https://github.com/broofa/node-uuid
+        uuid = function() {
+            var i = 0,
+                rnds = _rng(),
+                bth = _byteToHex;
+            rnds[6] = (rnds[6] & 0x0f) | 0x40;
             rnds[8] = (rnds[8] & 0x3f) | 0x80;
-			return bth[rnds[i++]] + bth[rnds[i++]] +
+            return bth[rnds[i++]] + bth[rnds[i++]] +
             bth[rnds[i++]] + bth[rnds[i++]] + '-' +
             bth[rnds[i++]] + bth[rnds[i++]] + '-' +
             bth[rnds[i++]] + bth[rnds[i++]] + '-' +
@@ -39,236 +40,235 @@ define('promise', ['$class'], function() {
             bth[rnds[i++]] + bth[rnds[i++]] +
             bth[rnds[i++]] + bth[rnds[i++]] +
             bth[rnds[i++]] + bth[rnds[i++]];
-		},
-		Promise;
-	doly.promiseCache = promiseCache;
-	doly.when = function() {
-	    return new Promise(doly.slice(arguments));
-	};
-	doly.defer = function() {
-	    return new Promise();
-	};
-	
-	Promise = doly.factory({
-	    
-		init: function(list) {
-		    var uid = uuid(),
-			    obj = promiseCache[uid] = {};
-			list || (list = [this]);
-			list.forEach(function(ps, i, list) {
-			    var tmpUid = ps.uid,
-				    data;
-				if (tmpUid) {
-				    data = promiseCache[tmpUid];
-					if ('resolveArgs' in data) {
-					    obj.resolveArgs = [];
-						obj.resolveArgs[i] = data.resolveArgs[0];
-					}
-					if ('rejectArgs' in data) {
-					    obj.rejectArgs = [];
-						obj.rejectArgs[i] = data.rejectArgs[0];
-					}
-				}
-				// 设置list中的每一个promise对象的uid为当前promise对象的uid
-				// 这样才能在需要的每一个promise对象的状态更改的时候都能够去
-				// 检测一次 是否能够执行成功或者失败的回调函数
-				if (ps !== this) {
-				    ps.references.push(this);
-				}
-			}, this);
-			obj.list = list;
-			// 所有的使用当前promise对象的promise对象uid集合
-			this.references = [];
-			this.uid = uid;
-		    this.state = PENDING;
-		},
-		
-		// 已完成
-		resolve: function(arg) {
-		    var data = promiseCache[this.uid],
-			    isAllResolved = true,
-				list, resolveArgs, resolves;
-			if (data) {
-			    this.state = RESOLVED;
-				this.resolveArg = arg;
-				resolveArgs = data.resolveArgs;
-				resolveArgs || (resolveArgs = data.resolveArgs = []);
-				list = data.list;
-				list.forEach(function(ps, i) {
-				    if (ps.state !== RESOLVED) {
-						isAllResolved = false;
-					}
-					resolveArgs[i] = ps.resolveArg;
-					// if (ps === this) {
-					    // resolveArgs[i] = arg;
-					// }
-				}, this);
-				resolves = data.resolves;
-				if (isAllResolved && resolves) {
-				    resolves.forEach(function(rs) {
-					    rs.apply(null, resolveArgs);
-					});
-				}
-			} else {
-			    doly.error('promise.resolve error: not initialized');
-			}
-			this.references.forEach(function(ps) {
-			    ps.resolve(ps.resolveArg);
-			});
-		},
-		
-		// 已拒绝
-		reject: function(arg) {
-		    var data = promiseCache[this.uid],
-			    isRejected = false,
-				list, rejectArgs, rejects;
-			// 确保只执行一次的失败处理函数
-			if (this.isRejected) return false;
-			if (data) {
-			    list = data.list;
-				this.rejectArg = arg;
-				rejectArgs = data.rejectArgs;
-				rejectArgs || (rejectArgs = data.rejectArgs = []);
-				list.forEach(function(ps, i) {
-				    if (ps.state === REJECTED) {
-					    isRejected = true;
-					}
-					rejectArgs[i] = ps.rejectArg;
-					if (ps === this) {
-						this.state = REJECTED;
-					}
-				}, this);
-				rejects = data.rejects;
-				// 确保失败回调执行 并且只执行一次
-				if ((!isRejected || !this.isRejected) && rejects) {
-				    rejects.forEach(function(rj) {
-					    rj.apply(null, rejectArgs);
-					});
-					this.isRejected = true; // 已经执行过已拒绝的处理函数
-				}
-			} else {
-			    doly.error('promise.reject error: not initialized');
-			}
-			this.references.forEach(function(ps) {
-			    ps.reject(ps.rejectArg);
-			});
-		},
-		
-		// 添加已完成和已拒绝的回调函数
-		then: function(resolvedHandler, rejectedHandler) {
-		    var data = promiseCache[this.uid],
-			    isAllResolved = true,
-				isRejected = false,
-				list;
-			if (data) {
-			    list = data.list;
-				list.forEach(function(lt) {
-				    var state = lt.state;
-					if (state !== RESOLVED) {
-						isAllResolved = false;
-					}
-					if (state === REJECTED) {
-						isRejected = true;
-					}
-				});
-				if (resolvedHandler) {
-					if (isAllResolved) {
-						resolvedHandler.apply(null, data.resolveArgs);
-					} else {
-						data.resolves || (data.resolves = []);
-						data.resolves.push(resolvedHandler);
-					}
-				}
-				if (rejectedHandler) {
-					if (isRejected) {
-						rejectedHandler.apply(null, data.rejectArgs);
-					} else {
-						data.rejects || (data.rejects = []);
-						data.rejects.push(rejectedHandler);
-					}
-				}
-			} else {
-			    doly.error('promise.then error: not initialized');
-			}
-			return this;
-			// this.done(resolvedHandler);
-			// this.fail(rejectedHandler);
-		},
-		
-		// 添加已完成的回调函数
-		done: function(resolvedHandler) {
-			var data = promiseCache[this.uid],
-			    isAllResolved = true,
-				list;
-			if (data) {
-			    list = data.list;
-				list.forEach(function(lt) {
-					if (lt.state !== RESOLVED) {
-						isAllResolved = false;
-					}
-				});
-				if (resolvedHandler) {
-					if (isAllResolved) {
-						resolvedHandler.apply(null, data.resolveArgs);
-					} else {
-						data.resolves || (data.resolves = []);
-						data.resolves.push(resolvedHandler);
-					}
-				}
-			} else {
-			    doly.error('promise.done error: not initialized');
-			}
-			return this;
-		},
-		
-		// 添加已拒绝的回调函数
-		fail: function(rejectedHandler) {
-			var data = promiseCache[this.uid],
-			    isRejected = false,
-				list;
-			if (data) {
-			    list = data.list;
-				list.forEach(function(lt) {
-					if (lt.state === REJECTED) {
-						isRejected = true;
-					}
-				});
-				if (rejectedHandler) {
-					if (isRejected) {
-						rejectedHandler.apply(null, data.rejectArgs);
-					} else {
-						data.rejects || (data.rejects = []);
-						data.rejects.push(rejectedHandler);
-					}
-				}
-			} else {
-			    doly.error('promise.fail error: not initialized');
-			}
-			return this;
-		},
-		
-		// 手动销毁
-		destory: function() {
-		    var uid = this.uid,
-			    cache = promiseCache[uid],
-				each = doly.each,
-				self = this;
-			// 延迟删除
-			setTimeout(function() {
-				each(cache, function(val, key) {
-					this[key] = val = null;
-					delete this[key];
-				}, cache);
-				promiseCache[uid] = null;
-				delete promiseCache[uid];
-				each(this, function(val, key) {
-					this[key] = val = null;
-					delete this[key];
-				}, self);
-			}, 0);
-		}
-		
-	});
-	
-	return Promise;
+        },
+        Promise;
+    doly.promiseCache = promiseCache;
+    doly.when = function() {
+        return new Promise(doly.slice(arguments));
+    };
+    doly.defer = function() {
+        return new Promise();
+    };
+    
+    Promise = doly.factory({
+        
+        init: function(list) {
+            var uid = uuid(),
+                obj = promiseCache[uid] = {};
+            list || (list = [this]);
+            list.forEach(function(ps, i, list) {
+                var tmpUid = ps.uid,
+                    data;
+                if (tmpUid) {
+                    data = promiseCache[tmpUid];
+                    if ('resolveArgs' in data) {
+                        obj.resolveArgs = [];
+                        obj.resolveArgs[i] = data.resolveArgs[0];
+                    }
+                    if ('rejectArgs' in data) {
+                        obj.rejectArgs = [];
+                        obj.rejectArgs[i] = data.rejectArgs[0];
+                    }
+                }
+                // 设置list中的每一个promise对象的uid为当前promise对象的uid
+                // 这样才能在需要的每一个promise对象的状态更改的时候都能够去
+                // 检测一次 是否能够执行成功或者失败的回调函数
+                if (ps !== this) {
+                    ps.references.push(this);
+                }
+            }, this);
+            obj.list = list;
+            // 所有的使用当前promise对象的promise对象uid集合
+            this.references = [];
+            this.uid = uid;
+            this.state = PENDING;
+        },
+        
+        // 已完成
+        resolve: function(arg) {
+            var data = promiseCache[this.uid],
+                isAllResolved = true,
+                list, resolveArgs, resolves;
+            if (data) {
+                this.state = RESOLVED;
+                this.resolveArg = arg;
+                resolveArgs = data.resolveArgs;
+                resolveArgs || (resolveArgs = data.resolveArgs = []);
+                list = data.list;
+                list.forEach(function(ps, i) {
+                    if (ps.state !== RESOLVED) {
+                        isAllResolved = false;
+                    }
+                    resolveArgs[i] = ps.resolveArg;
+                }, this);
+                resolves = data.resolves;
+                if (isAllResolved && resolves) {
+                    resolves.forEach(function(rs) {
+                        rs.apply(null, resolveArgs);
+                    });
+                    each(data, function(val, key) {
+                        this[key] = val = null;
+                        delete this[key];
+                    }, data);
+                    delete promiseCache[this.uid];
+                }
+            }
+            this.references.forEach(function(ps) {
+                ps.resolve(ps.resolveArg);
+            });
+        },
+        
+        // 已拒绝
+        reject: function(arg) {
+            var data = promiseCache[this.uid],
+                isRejected = false,
+                list, rejectArgs, rejects;
+            // 确保只执行一次的失败处理函数
+            if (this.isRejected) return false;
+            if (data) {
+                list = data.list;
+                this.rejectArg = arg;
+                rejectArgs = data.rejectArgs;
+                rejectArgs || (rejectArgs = data.rejectArgs = []);
+                list.forEach(function(ps, i) {
+                    if (ps.state === REJECTED) {
+                        isRejected = true;
+                    }
+                    rejectArgs[i] = ps.rejectArg;
+                    if (ps === this) {
+                        this.state = REJECTED;
+                    }
+                }, this);
+                rejects = data.rejects;
+                // 确保失败回调执行 并且只执行一次
+                if ((!isRejected || !this.isRejected) && rejects) {
+                    rejects.forEach(function(rj) {
+                        rj.apply(null, rejectArgs);
+                    });
+                    each(data, function(val, key) {
+                        this[key] = val = null;
+                        delete this[key];
+                    }, data);
+                    delete promiseCache[this.uid];
+                    this.isRejected = true; // 已经执行过已拒绝的处理函数
+                }
+            }
+            this.references.forEach(function(ps) {
+                ps.reject(ps.rejectArg);
+            });
+        },
+        
+        // 添加已完成和已拒绝的回调函数
+        then: function(resolvedHandler, rejectedHandler) {
+            var data = promiseCache[this.uid],
+                isAllResolved = true,
+                isRejected = false,
+                list;
+            if (data) {
+                list = data.list;
+                list.forEach(function(lt) {
+                    var state = lt.state;
+                    if (state !== RESOLVED) {
+                        isAllResolved = false;
+                    }
+                    if (state === REJECTED) {
+                        isRejected = true;
+                    }
+                });
+                if (resolvedHandler) {
+                    if (isAllResolved) {
+                        resolvedHandler.apply(null, data.resolveArgs);
+                    } else {
+                        data.resolves || (data.resolves = []);
+                        data.resolves.push(resolvedHandler);
+                    }
+                }
+                if (rejectedHandler) {
+                    if (isRejected) {
+                        rejectedHandler.apply(null, data.rejectArgs);
+                    } else {
+                        data.rejects || (data.rejects = []);
+                        data.rejects.push(rejectedHandler);
+                    }
+                }
+            }
+            return this;
+            // this.done(resolvedHandler);
+            // this.fail(rejectedHandler);
+        },
+        
+        // 添加已完成的回调函数
+        done: function(resolvedHandler) {
+            var data = promiseCache[this.uid],
+                isAllResolved = true,
+                list;
+            if (data) {
+                list = data.list;
+                list.forEach(function(lt) {
+                    if (lt.state !== RESOLVED) {
+                        isAllResolved = false;
+                    }
+                });
+                if (resolvedHandler) {
+                    if (isAllResolved) {
+                        resolvedHandler.apply(null, data.resolveArgs);
+                    } else {
+                        data.resolves || (data.resolves = []);
+                        data.resolves.push(resolvedHandler);
+                    }
+                }
+            }
+            return this;
+        },
+        
+        // 添加已拒绝的回调函数
+        fail: function(rejectedHandler) {
+            var data = promiseCache[this.uid],
+                isRejected = false,
+                list;
+            if (data) {
+                list = data.list;
+                list.forEach(function(lt) {
+                    if (lt.state === REJECTED) {
+                        isRejected = true;
+                    }
+                });
+                if (rejectedHandler) {
+                    if (isRejected) {
+                        rejectedHandler.apply(null, data.rejectArgs);
+                    } else {
+                        data.rejects || (data.rejects = []);
+                        data.rejects.push(rejectedHandler);
+                    }
+                }
+            }
+            return this;
+        },
+        
+        // 手动销毁
+        destory: function() {
+            var uid = this.uid,
+                cache = promiseCache[uid],
+                each = doly.each,
+                self = this;
+            // 延迟删除
+            setTimeout(function() {
+                if (cache) {
+                    each(cache, function(val, key) {
+                        this[key] = val = null;
+                        delete this[key];
+                    }, cache);
+                    promiseCache[uid] = null;
+                    delete promiseCache[uid];
+                }
+                each(this, function(val, key) {
+                    this[key] = val = null;
+                    delete this[key];
+                }, self);
+            }, 0);
+        }
+        
+    });
+    
+    return Promise;
 });
--- test/promise.js
+++ test/promise.js
@@ -43,32 +43,39 @@ $(function() {
 				return df;
 			};
 			fuc1().done(function(ret) {
-			    equal('fuc1:' + ret, 'fuc1:resolved1');
+			    doly.log('1');
+				equal('fuc1:' + ret, 'fuc1:resolved1');
 			}).fail(function(msg) {
 			    //equal('fuc1:fail:' + msg);
 			});
 			fuc2().done(function(ret) {
 			    //equal('fuc2:' + ret);
 			}).fail(function(msg) {
+			    doly.log('2');
 			    equal('fuc2:fail:' + msg, 'fuc2:fail:reject2');
 			});
 			fuc3().done(function(ret) {
-			    equal('fuc3:' + ret, 'fuc3:resolved3');
+			    doly.log('3');
+				equal('fuc3:' + ret, 'fuc3:resolved3');
 			}).fail(function(msg) {
 			    //equal('fuc3:fail:' + msg);
 			});
 			fuc4().then(function(ret) {
 			    //equal('fuc4:' + ret);
 			}, function(msg) {
-			    equal('fuc4:fail:' + msg, 'fuc4:fail:reject4');
+			    doly.log('4');
+				equal('fuc4:fail:' + msg, 'fuc4:fail:reject4');
 			});
 			fuc5().done(function(s1) {
-			    equal('fuc5:' + s1, 'fuc5:resolved5');
+			    doly.log('5');
+				equal('fuc5:' + s1, 'fuc5:resolved5');
 			});
 			fuc6().fail(function(s1) {
-			    equal('fuc6:fail:' + s1, 'fuc6:fail:reject6');
+			    doly.log('6');
+				equal('fuc6:fail:' + s1, 'fuc6:fail:reject6');
 			});
 			doly.when(fuc1(), fuc3()).done(function(r1, r3) {
+			    doly.log('7');
 			    equal('fuc1+fuc3:' + r1 + '+' + r3, 'fuc1+fuc3:resolved1+resolved3');
 			}).fail(function(r1, r2) {
 			    //equal('fuc1+fuc3:fail:' + r1 + '+' + r2);
@@ -76,16 +83,19 @@ $(function() {
 			doly.when(fuc2(), fuc4()).done(function(r1, r3) {
 			   // equal('fuc2+fuc4:' + r1 + '+' + r3);
 			}).fail(function(r1, r2) {
-			    equal('fuc2+fuc4:fail:' + r1 + '+' + r2, 'fuc2+fuc4:fail:reject2+undefined');
+			    doly.log('8');
+				equal('fuc2+fuc4:fail:' + r1 + '+' + r2, 'fuc2+fuc4:fail:reject2+undefined');
 			});
 			doly.when(fuc1(), fuc2()).then(function(r1, r3) {
 			    //equal('fuc1+fuc2:' + r1 + '+' + r3);
 			}, function(r1, r2) {
-			    equal('fuc1+fuc2:fail:' + r1 + '+' + r2, 'fuc1+fuc2:fail:undefined+reject2');
+			    doly.log('9');
+				equal('fuc1+fuc2:fail:' + r1 + '+' + r2, 'fuc1+fuc2:fail:undefined+reject2');
 			});
 			doly.when(fuc3(), fuc4()).then(function(r1, r3) {
 			    //equal('fuc3+fuc4:' + r1 + '+' + r3);
 			}, function(r1, r2) {
+			    doly.log('10');
 			    equal('fuc3+fuc4:fail:' + r1 + '+' + r2, 'fuc3+fuc4:fail:undefined+reject4');
 			});
 			var d1 = fuc1();
@@ -95,15 +105,19 @@ $(function() {
 			var d5 = fuc5();
 			var d6 = fuc6();
 			d1.done(function(s1) {
-			    equal('d1:' + s1, 'd1:resolved1');
+			    doly.log('11');
+				equal('d1:' + s1, 'd1:resolved1');
 			});
 			d2.fail(function(s1) {
-			    equal('d2:fail:' + s1, 'd2:fail:reject2');
+			    doly.log('12');
+				equal('d2:fail:' + s1, 'd2:fail:reject2');
 			});
 			d3.done(function(s1) {
-			    equal('d3:' + s1, 'd3:resolved3');
+			    doly.log('13');
+				equal('d3:' + s1, 'd3:resolved3');
 			});
 			d4.fail(function(s1) {
+			    doly.log('14');
 			    equal('d4:fail:' + s1, 'd4:fail:reject4');
 				d1.destory();
 				d2.destory();
@@ -116,17 +130,21 @@ $(function() {
 				}, 10);
 			});
 			d5.done(function(s1) {
-			    equal('d5:' + s1, 'd5:resolved5');
+			    doly.log('15');
+				equal('d5:' + s1, 'd5:resolved5');
 			});
 			d6.fail(function(s1) {
-			    equal('d6:fail:' + s1, 'd6:fail:reject6');
+			    doly.log('16');
+				equal('d6:fail:' + s1, 'd6:fail:reject6');
 			});
 			doly.when(d1, d2, d3, d4).then(function(r1, r2, r3, r4) {
 			   // equal('d1, d2, d3, d4:' + r1 + '+' + r2 + '+' + r3 + '+' + r4);
 			}, function(r1, r2, r3, r4) {
-			    equal('d1, d2, d3, d4:fail:' + r1 + '+' + r2 + '+' + r3 + '+' + r4, 'd1, d2, d3, d4:fail:undefined+reject2+undefined+undefined');
+			    doly.log('17');
+				equal('d1, d2, d3, d4:fail:' + r1 + '+' + r2 + '+' + r3 + '+' + r4, 'd1, d2, d3, d4:fail:undefined+reject2+undefined+undefined');
 			});
 			var a = doly.when(d1, d3).then(function(r1, r2) {
+			    doly.log('18');
 			    equal('d1, d3:' + r1 + '+' + r2, 'd1, d3:resolved1+resolved3');
 			}, function(r1, r2) {
 			   // equal('d1, d3:fail:' + r1 + '+' + r2);
@@ -134,23 +152,32 @@ $(function() {
 			doly.when(d4, d2).then(function(r1, r2) {
 			    //equal('d4, d2:' + r1 + '+' + r2);
 			}, function(r1, r2) {
-			    equal('d4, d2:fail:' + r1 + '+' + r2, 'd4, d2:fail:undefined+reject2');
+			    doly.log('19');
+				equal('d4, d2:fail:' + r1 + '+' + r2, 'd4, d2:fail:undefined+reject2');
 			});
 			doly.when(d1, d5).then(function(r1, r2) {
-			    equal('d1, d5:' + r1 + '+' + r2, 'd1, d5:resolved1+resolved5');
+			    doly.log('20');
+				equal('d1, d5:' + r1 + '+' + r2, 'd1, d5:resolved1+resolved5');
 			});
 			doly.when(d5, d6).fail(function(r1, r2) {
-			    equal('d5, d6:fail:' + r1 + '+' + r2, 'd5, d6:fail:undefined+reject6');
+			    doly.log('21');
+				equal('d5, d6:fail:' + r1 + '+' + r2, 'd5, d6:fail:undefined+reject6');
 			}).done(function(r1, r2) {
 			    //equal('d5, d6:' + r1 + '+' + r2);
 			});
 			doly.when(d2, d4, d6).fail(function(r1, r2, r3) {
+			    doly.log('22');
 			    equal('d2, d4, d6:fail:' + r1 + '+' + r2 + '+' + r3, 'd2, d4, d6:fail:undefined+undefined+reject6');
 			});
 			doly.when(d1, d2, d3, d4, d5, d6).then(function(r1, r2, r3, r4, r5, r6) {
 			    //equal('d1, d2, d3, d4, d5, d6:' + r1 + '+' + r2 + '+' + r3 + '+' + r4 + '+' + r5 + '+' + r6);
 			}, function(r1, r2, r3, r4, r5, r6) {
-			    equal('d1, d2, d3, d4, d5, d6:fail:' + r1 + '+' + r2 + '+' + r3 + '+' + r4 + '+' + r5 + '+' + r6, 'd1, d2, d3, d4, d5, d6:fail:undefined+undefined+undefined+undefined+undefined+reject6');
+			    doly.log('23');
+				equal('d1, d2, d3, d4, d5, d6:fail:' + r1 + '+' + r2 + '+' + r3 + '+' + r4 + '+' + r5 + '+' + r6, 'd1, d2, d3, d4, d5, d6:fail:undefined+undefined+undefined+undefined+undefined+reject6');
+			});
+			doly.when(d5).done(function(s) {
+			    doly.log('24');
+				equal('d5-when:' + s, 'd5-when:resolved5');
 			});
 			// start();
 		});
